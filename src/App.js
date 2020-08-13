b5045b39edf7570718a87541d758bdc5e723b3ea
Jakub Jureczko <untech.untech@gmail.com>
2020-08-13 17:35:30 +0200

    chabges

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import bd from './assets/k05.wav'
+import sn from './assets/s10.wav'
+import hh from './assets/h01.wav'
+import hh2 from './assets/c02.wav'
+import gt from './assets/guitar.wav'
+
+import useSound from 'use-sound';
+
+
 
 function App() {
+  
+     const [kick] = useSound(bd)
+     const [snare] = useSound(sn)
+     const [hihat] = useSound(hh)
+     const [hihat2] = useSound(hh2)
+     const [guitar] = useSound(gt)
+  
+// useEffect(() => document.addEventListener('keypress', (e) => handleKeyPress(e.code)),[])
+
+     
+    
+//     function handleKeyPress(keyCode) {
+//       switch (keyCode) {
+//         case 'KeyD':
+//           kick();      
+//         console.log('kick'); 
+//           break;
+//           case 'KeyF':
+//             snare(); 
+//         break;
+//         case 'KeyJ':
+//             hihat(); 
+//         break;
+//         case 'KeyK':
+//             hihat2(); 
+//         break;
+//         default:
+//           break;
+//       }
+//     }
+    
+ 
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+     
+            
+              <button onClick={kick}  >Kick D</button>
+              <button onClick={snare} >Snare F</button>
+              <button onClick={hihat} >Hat J</button>
+              <button onClick={hihat2} >Cymbal K</button>
+              <button onClick={guitar} >Guitar</button>
+        
     </div>
   );
 }
